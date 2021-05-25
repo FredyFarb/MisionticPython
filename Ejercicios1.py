8ddb55e18edab6418f6c820eadf36618728205f5
Fredy Ramírez <78213401+FredyFarb@users.noreply.github.com>
2021-05-24 22:12:36 -0500

    Add files via upload
    ejercicios Python upb, desarrollo y explicación

---
+++ Ejercicios1.py
@@ -0,0 +1,9 @@
+lado = init(input("ingresse la medida del lado del cuadrado"))
+superficie = lado*lado
+print("la superficie del cuadrado es")
+print(superficie)
+
+#ejercicio 2
+num1 = init(input("ingrese primer valor: "))
+
+
---
+++ holamundo.py
@@ -0,0 +1,18 @@
+print ("Hola mundo")
+
+print("bienvenido optenga la medida aritmética de 3 números")
+n1=int(input("ingrese un número: "))
+n2=int(input("ingrese un número: "))
+n3=int(input("ingrese un número: "))
+x=(n1+n2+n3)/3
+resultado=round(x,2)
+print(resultado)
+
+
+salario=y
+hora=x*(Vhora*8)
+Vhora=y/240
+x=int(input("ingrese el número de días trabajados: "))
+y=int(input("ingrese el valor del salario mensual: "))
+pago=hora*x
+ #no he terminado
